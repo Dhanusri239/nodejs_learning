1ac75057f92c549b17cb0a28b99dd7607ca2aad0
Dhanusri239 <sdhanu946@gmail.com>
2025-06-26 21:08:16 +0530

    program 3 is added

---
+++ prog3.js
@@ -0,0 +1,9 @@
+var name='sajiv';
+var age=12;
+var hobby='sleeping';
+function displayuser(username,userage,userhobby){
+    return "user name is " + username +'\n'
+    + "user age is " + userage + '\n' +
+    "user hobby is "+userhobby;
+}
+console.log(displayuser(name,age,hobby));
