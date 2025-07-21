40cc27a6afc4988d7157a396001f768d3a10b65b
Dhanusri239 <sdhanu946@gmail.com>
2025-07-21 22:26:45 +0530

    program 17 is added

---
+++ prog17.js
@@ -0,0 +1,45 @@
+const http=require('http');
+const fs=require('fs');
+const server=http.createServer((req,res)=>{
+    const url=req.url;
+    const method=req.method;
+    if(url=='/'){
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<title>Home page</title>');
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<form enctype="multipart/form-data" action="/message" method="POST"><input type =text name=message><input type = "file" name="file"><input type="submit" value="send"></form>');
+    res.write('</html>');
+    return res.end();
+    }
+    if(url==='/message' && method=='POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+           body.push(chunk);
+           console.log(chunk);
+        })
+        req.on('end',()=>{
+            const parsedbody=Buffer.concat(body).toString();
+            const message=parsedbody.split('=');
+            fs.writeFileSync("Text.txt",message[1]);
+        })
+
+       fs.writeFileSync("Hii.txt",'hiiii hello');
+       res.setHeader('Location','/');
+       res.statusCode=302;
+       return res.end();
+    }
+    res.setHeader('content-type','text/html');
+    res.write('<html>');
+        res.write('<head>');
+        res.write('<title>Home page</title>');
+        res.write('</head>');
+        res.write('<body>');
+        res.write('<h1>Welcome to our page</h1>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+
+});
+server.listen(3000);
