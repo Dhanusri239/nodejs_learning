e7d63c5324c09fff129c7acfa38dfb51c5355cf1
Dhanusri239 <sdhanu946@gmail.com>
2025-07-26 18:57:08 +0530

    Routes of 22th program is added

---
+++ Routes22.js
@@ -0,0 +1,48 @@
+
+const fs=require('fs');
+const requestHandler=(req,res)=>{
+    const url=req.url;
+    const method=req.method;
+    if(url=='/'){
+        res.write('<html>');
+        res.write('<head>');
+        res.write('<title>Home Page</title>');
+        res.write('</head>');
+        res.write('<body>');
+        res.write('<form action="/message" method ="POST"><input type="text" name="message"><input type="submit" value="send"></form>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/message' && method=='POST'){
+        const body=[];
+        req.on('data',(chunk)=>{
+          console.log('Chunk data');
+          body.push(chunk);
+        })
+        return req.on('end',()=>{
+                        console.log("End event received");
+                        const parsedbody=Buffer.concat(body).toString();
+                        const txt=parsedbody.split('=');
+                        fs.writeFile('Text1.txt',txt[1],(err)=>{
+                        console.log('File write is completed');
+                         res.setHeader('Location','/');
+                         res.statusCode=302;
+                         return res.end();   
+                    });
+                })
+       
+    }
+      res.setHeader('content-type','text/html');
+         res.write('<html>');
+             res.write('<head>');
+             res.write('<title>Home page</title>');
+             res.write('</head>');
+             res.write('<body>');
+             res.write('<h1>Welcome to our page</h1>');
+             res.write('</body>');
+             res.write('</html>');
+             res.end();
+}
+exports.handler=requestHandler;
+exports.someText='printing some values';
