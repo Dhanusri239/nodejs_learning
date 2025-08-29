c7868d088fe7a88d1f82e0eeee7f485caaabb377
Dhanusri239 <sdhanu946@gmail.com>
2025-08-29 20:35:07 +0530

    prog40.js is added

---
+++ Expressjs/prog40.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const val=express();
+const path=require('path');
+const bodyparser=require('body-parser');
+const adminRoutes=require('./Routes/admin');
+const shopRoutes=require('./Routes/shop');
+val.set('view engine','pug');
+val.use('/admin',adminRoutes);
+val.use(shopRoutes);
+val.use((req,res,next)=>{
+    res.status(404).render('404',{
+        docTitle:"Page not Found"
+    });
+});
+val.listen(8000);
